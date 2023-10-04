7a4fd41d15e73085224655ae4dd260bfee66fb8f
Palate Jean <jean.palate@nbb.be>
2023-10-04 18:17:15 +0200

    main functions

--- R/jd3txt.R
+++ R/jd3txt.R
@@ -1,44 +1,60 @@
 #' @include providers.R
 
-.txt_source<-function(file, fmt.locale=NULL, fmt.date=NULL, fmt.number=NULL, fmt.ignore_grouping=T, 
-                      gathering.period=0, gathering.agg=c("Sum", "Average", "first", "Last", "Max", "Min"), gathering.partial=F, gathering.nomissing=T,
-                      charset=NULL, delimiter=c('TAB', 'SEMICOLON', 'COMMA', 'SPACE'), txtqualifier=c('NONE', 'QUOTE', 'DOUBLE_QUOTE'), header=T, skip=0){
-  gathering.agg=match.arg(gathering.agg)
+.txt_source<-function(file, fmt.locale=NULL, fmt.date=NULL, fmt.number=NULL, fmt.ignoreNumberGrouping=T, 
+                      gathering.period=0, gathering.aggregation=c("None", "Sum", "Average", "First", "Last", "Max", "Min"), gathering.partialAggregation=F, gathering.includeMissing=F,
+                      charset=NULL, delimiter=c('TAB', 'SEMICOLON', 'COMMA', 'SPACE'), textQualifier=c('NONE', 'QUOTE', 'DOUBLE_QUOTE'), headers=T, skipLines=0){
+  gathering.aggregation=match.arg(gathering.aggregation)
   delimiter<-match.arg(delimiter)
-  txtqualifier<-match.arg(txtqualifier)
-  jfmt<-.obs_format(fmt.locale, fmt.date, fmt.number, fmt.ignore_grouping)
-  jgathering<-.obs_gathering(gathering.period, gathering.agg, gathering.partial, gathering.nomissing)
+  textQualifier<-match.arg(textQualifier)
+  jfmt<-.obs_format(fmt.locale, fmt.date, fmt.number, fmt.ignoreNumberGrouping)
+  jgathering<-.obs_gathering(gathering.period, gathering.aggregation, gathering.partialAggregation, gathering.includeMissing)
   if (is.null(charset)) charset<-'utf-8'
   jsource<-.jcall("jdplus/text/base/r/TxtFiles", "Ljdplus/toolkit/base/tsp/DataSource;", "source", 
                   as.character(file), jfmt, jgathering, as.character(charset), as.character(delimiter), 
-                  as.character(txtqualifier), as.logical(header), as.integer(skip))
+                  as.character(textQualifier), as.logical(headers), as.integer(skipLines))
   return (jsource)
 }
 
 .r2jd_txt_id<-function(id){
-  agg<-id$gathering$aggregation
-  if (is.null(agg)) agg<-""
-  jsrc<-.spreadsheet_source(id$file, id$gathering$period, agg, id$gathering$partial, id$gathering$missing)
+  jsrc<-.txt_source(file=id$file, fmt.locale = id$format$locale, fmt.date = id$format$datePattern,
+                    fmt.number = id$format$numberPattern, fmt.ignoreNumberGrouping = id$format$ignoreNumberGrouping, 
+                    gathering.period = id$gathering$period, gathering.aggregation = id$gathering$aggregation, gathering.partialAggregation = id$gathering$partial, gathering.includeMissing = id$gathering$missing,
+                    charset = id$charset, delimiter = id$delimiter, textQualifier = id$textQualifier, headers = id$headers, skipLines = id$skipLines)
   if (is.null(id$series))
-    return (.jcall("jdplus/spreadsheet/base/r/SpreadSheets",  "Ljdplus/toolkit/base/tsp/DataSet;", "sheetDataSet", jsrc, id$sheet))
+    return (.jcall("jdplus/text/base/r/TxtFiles",  "Ljdplus/toolkit/base/tsp/DataSet;", "sheetDataSet", jsrc))
   else
-    return (.jcall("jdplus/spreadsheet/base/r/SpreadSheets",  "Ljdplus/toolkit/base/tsp/DataSet;", "seriesDataSet", jsrc, id$sheet, id$series))
+    return (.jcall("jdplus/text/base/r/TxtFiles",  "Ljdplus/toolkit/base/tsp/DataSet;", "seriesDataSet", jsrc, as.integer(id$series)))
 }
 
 .jd2r_txt_id<-function(jset){
-  jbean<-.jcall("jdplus/spreadsheet/base/r/SpreadSheets","Ljdplus/spreadsheet/base/api/SpreadSheetBean;", "sourceOf", jset)
+  jbean<-.jcall("jdplus/text/base/r/TxtFiles","Ljdplus/text/base/api/TxtBean;", "sourceOf", jset)
   jfile<-.jcall(jbean, "Ljava/io/File;", "getFile")
+  jcharset<-.jcall(jbean, "Ljava/nio/charset/Charset;", "getCharset")
+  jformat<-.jcall(jbean, "Ljdplus/toolkit/base/tsp/util/ObsFormat;", "getFormat")
   jgathering<-.jcall(jbean, "Ljdplus/toolkit/base/api/timeseries/util/ObsGathering;", "getGathering")
   junit<-.jcall(jgathering, "Ljdplus/toolkit/base/api/timeseries/TsUnit;", "getUnit" )
   jagg<-.jcall(jgathering, "Ljdplus/toolkit/base/api/data/AggregationType;", "getAggregationType" )
+  jlocale<-.jcall(jformat, "Ljava/util/Locale;", "getLocale")
+  jdelimiter<-.jcall(jbean, "Ljdplus/text/base/api/TxtBean$Delimiter;", "getDelimiter" )
+  jqual<-.jcall(jbean, "Ljdplus/text/base/api/TxtBean$TextQualifier;", "getTextQualifier" )
   return (list(
     file=.jcall(jfile, "S", "getName"),
-    sheet=.jcall(jset, "S", "getParameter", "sheetName"),
-    series=.jcall(jset, "S", "getParameter", "seriesName"),
+    delimiter = .jcall(jdelimiter, "S", "name"),
+    textQualifier = .jcall(jqual, "S", "name"),
+    headers = as.integer(.jcall(jbean, "Z", "isHeaders")),
+    skipLines = as.integer(.jcall(jbean, "I", "getSkipLines")),
+    series=1+as.integer(.jcall(jset, "S", "getParameter", "seriesIndex")),
+    
     gathering=list(period=.jcall(junit, "I", "getAnnualFrequency"), 
                    aggregation=.jcall(jagg, "S", "name"), 
-                   partial=.jcall(jgathering, "Z", "isAllowPartialAggregation"), 
-                   missing=.jcall(jgathering, "Z", "isIncludeMissingValues")  )
+                   partialAggregation=.jcall(jgathering, "Z", "isAllowPartialAggregation"), 
+                   includeMissing=.jcall(jgathering, "Z", "isIncludeMissingValues")),
+    format=list(
+      locale=.jcall(jlocale, "S", "toLanguageTag"),
+      datePattern=.jcall(jformat, "S", "getDateTimePattern"),
+      numberPattern=.jcall(jformat, "S", "getNumberPattern"),
+      ignoreNumberGrouping=.jcall(jformat, "Z", "isIgnoreNumberGrouping")
+    )
   ))
 }
 
@@ -83,14 +99,14 @@ set_txt_paths<-function(paths){
 #' @param fmt.locale 
 #' @param fmt.date 
 #' @param fmt.number 
-#' @param fmt.ignore_grouping 
+#' @param fmt.ignoreNumberGrouping 
 #' @param gathering.period 
-#' @param gathering.agg 
-#' @param gathering.partial 
-#' @param gathering.nomissing 
+#' @param gathering.aggregation 
+#' @param gathering.partialAggregation 
+#' @param gathering.includeMissing 
 #' @param charset 
 #' @param delimiter 
-#' @param txtqualifier 
+#' @param txtQualifier 
 #' @param header 
 #' @param skip 
 #'
@@ -100,12 +116,12 @@ set_txt_paths<-function(paths){
 #' @examples
 #' set_txt_paths(system.file("examples", package="rjd3providers"))
 #' txt_all<-txt_content("abs.csv", delimiter = 'COMMA')
-txt_content<-function(file, fmt.locale=NULL, fmt.date=NULL, fmt.number=NULL, fmt.ignore_grouping=T, 
-                      gathering.period=0, gathering.agg=c("Sum", "Average", "first", "Last", "Max", "Min"), gathering.partial=F, gathering.nomissing=T,
-                      charset=NULL, delimiter=c('TAB', 'SEMICOLON', 'COMMA', 'SPACE'), txtqualifier=c('NONE', 'QUOTE', 'DOUBLE_QUOTE'), header=T, skip=0){
-  jsource<-.txt_source(file, fmt.locale, fmt.date, fmt.number, fmt.ignore_grouping, 
-                       gathering.period, gathering.agg, gathering.partial, gathering.nomissing,
-                       charset, delimiter, txtqualifier, header, skip)
+txt_content<-function(file, fmt.locale=NULL, fmt.date=NULL, fmt.number=NULL, fmt.ignoreNumberGrouping=T, 
+                      gathering.period=0, gathering.aggregation=c("None", "Sum", "Average", "First", "Last", "Max", "Min"), gathering.partialAggregation=F, gathering.includeMissing=T,
+                      charset=NULL, delimiter=c('TAB', 'SEMICOLON', 'COMMA', 'SPACE'), txtQualifier=c('NONE', 'QUOTE', 'DOUBLE_QUOTE'), header=T, skip=0){
+  jsource<-.txt_source(file, fmt.locale, fmt.date, fmt.number, fmt.ignoreNumberGrouping, 
+                       gathering.period, gathering.aggregation, gathering.partialAggregation, gathering.includeMissing,
+                       charset, delimiter, txtQualifier, header, skip)
   series<-.jcall("jdplus/text/base/r/TxtFiles","[S", "series", jsource)
   return (series)
 }
@@ -116,14 +132,14 @@ txt_content<-function(file, fmt.locale=NULL, fmt.date=NULL, fmt.number=NULL, fmt
 #' @param fmt.locale 
 #' @param fmt.date 
 #' @param fmt.number 
-#' @param fmt.ignore_grouping 
+#' @param fmt.ignoreNumberGrouping 
 #' @param gathering.period 
-#' @param gathering.agg 
-#' @param gathering.partial 
-#' @param gathering.nomissing 
+#' @param gathering.aggregation 
+#' @param gathering.partialAggregation 
+#' @param gathering.includeMissing 
 #' @param charset 
 #' @param delimiter 
-#' @param txtqualifier 
+#' @param txtQualifier 
 #' @param header 
 #' @param skip 
 #'
@@ -133,12 +149,12 @@ txt_content<-function(file, fmt.locale=NULL, fmt.date=NULL, fmt.number=NULL, fmt
 #' @examples
 #' set_txt_paths(system.file("examples", package="rjd3providers"))
 #' all<-txt_data("abs.csv", delimiter = 'COMMA')
-txt_data<-function(file, fmt.locale=NULL, fmt.date=NULL, fmt.number=NULL, fmt.ignore_grouping=T, 
-                   gathering.period=0, gathering.agg=c("Sum", "Average", "first", "Last", "Max", "Min"), gathering.partial=F, gathering.nomissing=T,
-                   charset=NULL, delimiter=c('TAB', 'SEMICOLON', 'COMMA', 'SPACE'), txtqualifier=c('NONE', 'QUOTE', 'DOUBLE_QUOTE'), header=T, skip=0){
-  jsource<-.txt_source(file, fmt.locale, fmt.date, fmt.number, fmt.ignore_grouping, 
-                       gathering.period, gathering.agg, gathering.partial, gathering.nomissing,
-                       charset, delimiter, txtqualifier, header, skip)
+txt_data<-function(file, fmt.locale=NULL, fmt.date=NULL, fmt.number=NULL, fmt.ignoreNumberGrouping=T, 
+                   gathering.period=0, gathering.aggregation=c("None", "Sum", "Average", "First", "Last", "Max", "Min"), gathering.partialAggregation=F, gathering.includeMissing=T,
+                   charset=NULL, delimiter=c('TAB', 'SEMICOLON', 'COMMA', 'SPACE'), txtQualifier=c('NONE', 'QUOTE', 'DOUBLE_QUOTE'), header=T, skip=0){
+  jsource<-.txt_source(file, fmt.locale, fmt.date, fmt.number, fmt.ignoreNumberGrouping, 
+                       gathering.period, gathering.aggregation, gathering.partialAggregation, gathering.includeMissing,
+                       charset, delimiter, txtQualifier, header, skip)
   jcoll<-.jcall("jdplus/text/base/r/TxtFiles", "Ljdplus/toolkit/base/api/timeseries/TsCollection;", "collection", jsource)
   return (rjd3toolkit::.jd2r_tscollection(jcoll))
 }
@@ -150,14 +166,14 @@ txt_data<-function(file, fmt.locale=NULL, fmt.date=NULL, fmt.number=NULL, fmt.ig
 #' @param fmt.locale 
 #' @param fmt.date 
 #' @param fmt.number 
-#' @param fmt.ignore_grouping 
+#' @param fmt.ignoreNumberGrouping 
 #' @param gathering.period 
-#' @param gathering.agg 
-#' @param gathering.partial 
-#' @param gathering.nomissing 
+#' @param gathering.aggregation 
+#' @param gathering.partialAggregation 
+#' @param gathering.includeMissing 
 #' @param charset 
 #' @param delimiter 
-#' @param txtqualifier 
+#' @param txtQualifier 
 #' @param header 
 #' @param skip 
 #'
@@ -167,12 +183,12 @@ txt_data<-function(file, fmt.locale=NULL, fmt.date=NULL, fmt.number=NULL, fmt.ig
 #' @examples
 #' set_txt_paths(system.file("examples", package="rjd3providers"))
 #' txt_5<-txt_series("abs.csv", series=15, delimiter = 'COMMA')
-txt_series<-function(file, series, fmt.locale=NULL, fmt.date=NULL, fmt.number=NULL, fmt.ignore_grouping=T, 
-                     gathering.period=0, gathering.agg=c("Sum", "Average", "first", "Last", "Max", "Min"), gathering.partial=F, gathering.nomissing=T,
-                     charset=NULL, delimiter=c('TAB', 'SEMICOLON', 'COMMA', 'SPACE'), txtqualifier=c('NONE', 'QUOTE', 'DOUBLE_QUOTE'), header=T, skip=0){
-  jsource<-.txt_source(file, fmt.locale, fmt.date, fmt.number, fmt.ignore_grouping, 
-                       gathering.period, gathering.agg, gathering.partial, gathering.nomissing,
-                       charset, delimiter, txtqualifier, header, skip)
+txt_series<-function(file, series, fmt.locale=NULL, fmt.date=NULL, fmt.number=NULL, fmt.ignoreNumberGrouping=T, 
+                     gathering.period=0, gathering.aggregation=c("None", "Sum", "Average", "First", "Last", "Max", "Min"), gathering.partialAggregation=F, gathering.includeMissing=T,
+                     charset=NULL, delimiter=c('TAB', 'SEMICOLON', 'COMMA', 'SPACE'), txtQualifier=c('NONE', 'QUOTE', 'DOUBLE_QUOTE'), header=T, skip=0){
+  jsource<-.txt_source(file, fmt.locale, fmt.date, fmt.number, fmt.ignoreNumberGrouping, 
+                       gathering.period, gathering.aggregation, gathering.partialAggregation, gathering.includeMissing,
+                       charset, delimiter, txtQualifier, header, skip)
   js<-.jcall("jdplus/text/base/r/TxtFiles", "Ljdplus/toolkit/base/api/timeseries/Ts;", "series", jsource, as.integer(series))
   return (rjd3toolkit::.jd2r_ts(js))
 }
@@ -204,5 +220,20 @@ txt_id_properties<-function(id){
   return (.jd2r_txt_id(jset))
 }
 
+#' Title
+#'
+#' @param id Identifier of the series (from its moniker)
+#' @param nfile New file name
+#' @param ofile Old file name. NULL or "" to change any file to the new file
+#'
+#' @return
+#' @export
+#'
+#' @examples
+txt_change_file<-function(id, nfile, ofile=NULL){
+  if (is.null(ofile)) ofile=""
+  nid<-.jcall("jdplus/text/base/r/TxtFiles", "S", "changeFile", id, nfile, ofile)
+  return (nid)
+}
 
 
