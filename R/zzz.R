a85b08b985641d9c485f625dbf5e8f0770dca2f1
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2026-09-11 11:02:58 +0000

    add @AlessandroPiovani as cph

--- R/zzz.R
+++ R/zzz.R
@@ -46,34 +46,3 @@ NULL
         rjd3jars::reload_tsproviders()
     }
 }
-
-#' Set an option for providers
-#'
-#' @param name Name of the option
-#' @param obj Option
-#'
-#' @export
-#'
-#' @examples
-#' providers_option("test", "DUMMY")
-providers_option <- function(name, obj) {
-    rjd3providers_options <- rjd3toolkit::.jd3_env$providers
-    rjd3providers_options[[name]] <- obj
-    assign("providers", rjd3providers_options, rjd3toolkit::.jd3_env)
-    invisible()
-}
-
-#' Set an option for providers
-#'
-#' @param name Name of the option
-#'
-#' @returns The requested option or NULL if it doesn't exist
-#' @export
-#'
-#' @examples
-#' providers_option("test", "DUMMY")
-#' get_providers_option("test")
-get_providers_option <- function(name) {
-    rjd3providers_options <- rjd3toolkit::.jd3_env$providers
-    return(rjd3providers_options[[name]])
-}
